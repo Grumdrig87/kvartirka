d94842bd72f054c25c95ce802e2c91395bef371a
Vit <grumdrig@mail.ru>
2021-05-17 20:54:35 +0300

    1

--- js/main.js
+++ js/main.js
@@ -24,12 +24,22 @@ jQuery(document).ready(function($) {
     $(this).hide();
   });
 // slide to id
+if (jQuery('[data-flatnav]').length > 0) {
   $('[data-flatnav]').on("click","a", function (event) {
     event.preventDefault();
     var id  = $(this).attr('href'),
         top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 1500);
   });
+}
+if (jQuery('[data-addnew]').length > 0) {
+  $('[data-addnew]').on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+}
 //adaptive
 
   
@@ -96,6 +106,11 @@ jQuery(document).ready(function($) {
       width: '100%'
     });
   }
+  if (jQuery('[data-addnewsel]').length > 0) {
+    jQuery('[data-addnewsel]').select2({
+      width: '100%'
+    });
+  }
   if (jQuery('[data-time]').length > 0) {
     jQuery('[data-time]').select2({
       width: 'auto',
@@ -103,6 +118,7 @@ jQuery(document).ready(function($) {
       dropdownAutoWidth: true
     });
   }
+  
   if (jQuery('[data-val]').length > 0) {
     jQuery('[data-val]').select2({
       width: 'auto',
@@ -205,7 +221,7 @@ jQuery(document).ready(function($) {
   $('[data-mguest]').click(function(){
     $('[data-guestdrop]').toggleClass('active');
   })
-
+// miss click
   function closeMenu () {
     $(document).mouseup(function (e){ // событие клика по веб-документу
       var div = $(".main__guest"); // тут указываем ID элемента
@@ -255,7 +271,7 @@ jQuery(document).ready(function($) {
     } );
 
 
-
+//foto load
     $("#proffoto").change(function () {
       if (this.files && this.files[0]) {
           var reader = new FileReader();
@@ -265,7 +281,70 @@ jQuery(document).ready(function($) {
           reader.readAsDataURL(this.files[0]);
       }
   });
+
+  //dropzone foto
+  var dropZone = $('#dropZone'),
+        maxFileSize = 1000000; // максимальный размер фалйа - 1 мб.
     
+    // Проверка поддержки браузером
+    if (typeof(window.FileReader) == 'undefined') {
+        dropZone.text('Не поддерживается браузером!');
+        dropZone.addClass('error');
+    }
+    
+    // Добавляем класс hover при наведении
+    dropZone[0].ondragover = function() {
+        dropZone.addClass('hover');
+        return false;
+    };
+    
+    // Убираем класс hover
+    dropZone[0].ondragleave = function() {
+        dropZone.removeClass('hover');
+        return false;
+    };
+    
+    // Обрабатываем событие Drop
+    dropZone[0].ondrop = function(event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+        
+        var file = event.dataTransfer.files[0];
+        
+        // Проверяем размер файла
+        if (file.size > maxFileSize) {
+            dropZone.text('Файл слишком большой!');
+            dropZone.addClass('error');
+            return false;
+        }
+        
+        // Создаем запрос
+        var xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener('progress', uploadProgress, false);
+        xhr.onreadystatechange = stateChange;
+        xhr.open('POST', '/upload.php');
+        xhr.setRequestHeader('X-FILE-NAME', file.name);
+        xhr.send(file);
+    };
+    
+    // Показываем процент загрузки
+    function uploadProgress(event) {
+        var percent = parseInt(event.loaded / event.total * 100);
+        dropZone.text('Загрузка: ' + percent + '%');
+    }
+    
+    // Пост обрабочик
+    function stateChange(event) {
+        if (event.target.readyState == 4) {
+            if (event.target.status == 200) {
+                dropZone.text('Загрузка успешно завершена!');
+            } else {
+                dropZone.text('Произошла ошибка!');
+                dropZone.addClass('error');
+            }
+        }
+    }
 });
 
 
